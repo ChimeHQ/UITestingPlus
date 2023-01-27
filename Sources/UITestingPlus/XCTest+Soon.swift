755f1871d0f44aef160aa0d8e8dd8374a778d86d
Chris Vasselli <clindsay@gmail.com>
2023-01-27 10:04:27 +0900

    Add various utilities for performing actions "soon" (default 3 seconds).
    Given the sometimes flaky nature of the "wait for app to idle" mechanism, using these functions in place of their standard counterparts is a good way to reduce flaky test failures.

---
+++ Sources/UITestingPlus/XCTest+Soon.swift
@@ -0,0 +1,65 @@
+import Foundation
+import XCTest
+
+
+/// The default amount of time to wait for functions and variables with "soon" in their name. 3 seconds.
+public enum XCTestSoon {
+    public static let defaultWaitTime: TimeInterval = 3
+}
+
+/// Returns true if `expression` evaluates to true within `waitTime` (default 3 seconds)
+public func IsTrueSoon(_ expression: @autoclosure () throws -> Bool, waitTime: TimeInterval = XCTestSoon.defaultWaitTime) -> Bool {
+    let startTime = Date()
+    repeat {
+        let success = (try? expression()) ?? false
+        if success {
+            return true
+        }
+        else {
+            Thread.sleep(forTimeInterval: 1)
+        }
+    }
+    while Date().timeIntervalSince(startTime) < waitTime
+    
+    return (try? expression()) ?? false
+}
+
+/// Drop-in replacement for `XCTAssert` that gives `waitTime` (default 3 seconds) for `expression` to evaluate to true
+public func XCTAssertSoon(_ expression: @autoclosure () throws -> Bool, _ message: @autoclosure () -> String = "", file: StaticString = #filePath, line: UInt = #line, waitTime: TimeInterval = XCTestSoon.defaultWaitTime) {
+    XCTAssert(IsTrueSoon(try expression(), waitTime: waitTime), message(), file: file, line: line)
+}
+
+/// Drop-in replacement for XCTUnwrap that gives `waitTime` (default 3 seconds) for `expression` to evaluate to non-nil
+public func XCTUnwrapSoon<T>(_ expression: @autoclosure () throws -> T?, _ message: @autoclosure () -> String = "", file: StaticString = #filePath, line: UInt = #line, waitTime: TimeInterval = XCTestSoon.defaultWaitTime) throws -> T {
+    let startTime = Date()
+    repeat {
+        if let result = try? expression() {
+            return result
+        }
+        else {
+            Thread.sleep(forTimeInterval: 1)
+        }
+    }
+    while Date().timeIntervalSince(startTime) < waitTime
+    
+    return try XCTUnwrap(expression(), message(), file: file, line: line)
+}
+
+/// Drop-in replacement for `XCTAssertEqual` that gives `waitTime` (default 3 seconds) for `expression1` to evaluate to equal to `expression2`
+public func XCTAssertEqualSoon<T>(_ expression1: @autoclosure () throws -> T, _ expression2: @autoclosure () throws -> T, _ message: @autoclosure () -> String = "", file: StaticString = #filePath, line: UInt = #line, waitTime: TimeInterval = XCTestSoon.defaultWaitTime) where T : Equatable {
+    if IsTrueSoon((try? expression1() == (try? expression2())) ?? false, waitTime: waitTime) {
+        return
+    }
+    
+    XCTAssertEqual(try? expression1(), try? expression2(), message(), file: file, line: line)
+
+}
+
+/// Drop-in replacement for `XCTAssertNotEqual` that gives `waitTime` (default 3 seconds) for `expression1` to evaluate not equal to `expression2`
+public func XCTAssertNotEqualSoon<T>(_ expression1: @autoclosure () throws -> T, _ expression2: @autoclosure () throws -> T, _ message: @autoclosure () -> String = "", file: StaticString = #filePath, line: UInt = #line, waitTime: TimeInterval = XCTestSoon.defaultWaitTime) where T : Equatable {
+    if IsTrueSoon((try? expression1() != (try? expression2())) ?? false, waitTime: waitTime) {
+        return
+    }
+
+    XCTAssertNotEqual(try? expression1(), try? expression2(), message(), file: file, line: line)
+}
---
+++ Sources/UITestingPlus/XCUIElement+Hittable.swift
@@ -0,0 +1,27 @@
+import Foundation
+import XCTest
+
+extension XCUIElement {
+    
+    /// Returns true if the element's `isHittable` property is true within `timeout` seconds
+    func waitForHittable(timeout: TimeInterval) -> Bool {
+        let predicate = NSPredicate(format: "isHittable == 1")
+        let exp = XCTNSPredicateExpectation(predicate: predicate, object: self)
+        
+        let result = XCTWaiter.wait(for: [exp], timeout: timeout)
+        
+        return result == .completed
+    }
+    
+    /// Returns true if the element's `isHittable` property is false within `timeout` seconds
+    func waitForNotHittable(timeout: TimeInterval) -> Bool {
+        let predicate = NSPredicate(format: "isHittable == 0")
+        let exp = XCTNSPredicateExpectation(predicate: predicate, object: self)
+        
+        let result = XCTWaiter.wait(for: [exp], timeout: timeout)
+        
+        return result == .completed
+    }
+    
+    
+}
